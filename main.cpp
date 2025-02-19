00232c5287964bffbe22bcc7f68306cc42aee243
Kevin Johnsson <kevin.torneus@elev.ga.ntig.se>
2025-02-19 13:48:26 +0100

    nearly complete drop dead with multiple players yes skbidi

--- main.cpp
+++ main.cpp
@@ -5,17 +5,24 @@ using namespace std;
 float blab = 0;
 int dice = 0;
 
+int turn = 0;
+int numberOfPlayers;
+
+string playerNames[] = {"", "", "", "", "", "", "", "" ,"", ""};
+
 int playerDice[] = {0, 0, 0, 0, 0}; 
 int puterDice[] = {0, 0, 0, 0, 0}; 
 
-int remainingPlayerDice = 5;
+int remainingPlayerDice[] = {5, 5, 5, 5 ,5 ,5 ,5 ,5, 5, 5};
 int remainingPuterDice = 5;
 
 bool playerDead = false;
 bool puterDead = false;
 
-int playerTotal = 0;
+int playerTotal[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int puterTotal = 0;
+
+bool gameEnded;
 
 int randomNum(int lowerLim, int upperLim){
   int final = lowerLim + (rand() % upperLim);
@@ -45,28 +52,76 @@ int main() {
   cout << "Hello World!" << "\n";
   cout << "https://en.wikipedia.org/wiki/Drop_Dead_(dice_game)" << endl;
   cout << "You are playing drop dead" << endl;
+
+  cout << "How many players???? MAX: 10!!!" << endl;
+  
+  cin >> numberOfPlayers;
+  cout << "There are: " << numberOfPlayers << " players!" << endl;
+  cout << "Type player names" << endl;
+  for(int i = 0; i < numberOfPlayers; i++){
+    string name;
+    cin >> name;
+    playerNames[i] = name;
+
+  }
+  for(int i = 0; i < numberOfPlayers; i++){
+    cout << "Player " << i+1 << ": " << playerNames[i] << endl;
+
+  }
+
+
   //int val;
   //cin >> val;
   //cout << "Now there are " << val << " blabs!" << endl;
 
-  int currentRemain = remainingPlayerDice;
-  for(int i = 0; i < currentRemain; i++){
-    int current = rollDice();
-    playerDice[i] = current;
-    cout << current << endl;
-  }
-  for(int i = 0; i < currentRemain; i++){
-    int current = playerDice[i];
-    cout << "Dice " << i+1 << ": " <<  current << endl;
-    if(current == 2 || current == 5){
-      remainingPlayerDice --;
-    }else{
-      playerTotal += current;
-    }
-  }
-  cout << "Player is now left with: " << remainingPlayerDice << " dice!" << endl;
-  cout << "Player total score is now: " << playerTotal << "!" << endl;
+  while(!gameEnded)
+  {
+    cout << "Turn: " << turn+1 << endl;
+    cout << "Rolling for " << playerNames[turn] << endl;
+    while(remainingPlayerDice[turn] > 0){
+      int currentRemain = remainingPlayerDice[turn];
   
+      for(int i = 0; i < currentRemain; i++){
+        int current = rollDice();
+        playerDice[i] = current;
+        cout << "Rolled " << current << endl;
+      }
+      for(int i = 0; i < currentRemain; i++){
+        int current = playerDice[i];
+        cout << "Dice " << i+1 << ": " <<  current << endl;
+        if(current == 2 || current == 5){
+          remainingPlayerDice[turn] --;
+        }else{
+          playerTotal[turn] += current;
+        }
+      }
+      
+      cout << playerNames[turn] << " is now left with: " << remainingPlayerDice[turn] << " dice!" << endl;
+      cout << playerNames[turn] << " total score is now: " << playerTotal[turn] << "!" << endl;
+  
+    }
+
+    remainingPlayerDice[turn] = 5;
+
+    cout << playerNames[turn] << " dropped dead!" << endl;
+
+    turn++;
+
+    if(turn >= numberOfPlayers){
+      gameEnded = true;
+    }
+
+    
+
+    
+  }
+  cout << "Game Ended" << endl;
+  for(int i = 0; i < numberOfPlayers; i++){
+    cout << playerNames[i] << "'s final score: " << playerTotal[i] << endl;
+  }
+
+ 
+ 
   
   
   return 0;
